b071fd0d58565a27b7379363add9771dd257ceba
James Lopez <jl71491@gmail.com>
2017-10-26 14:33:15 -0700

    Created function to dynamically create tooltips.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # tooltip
 A website with tooltips for people who want to have a better understanding of what their interactions do.
+
+## Useage
+```bash
+$ npm install
+$ npm run
+```
+
+## Uses
+* HTML5
+* CSS3
+* Modern JavaScript (ES2015+)
+* Express
+* Node.js
+
+## Screenshot
--- public/main.js
+++ public/main.js
@@ -1,14 +1,25 @@
 const $tooltipped = document.querySelectorAll('.tooltipped')
 
 $tooltipped.forEach($tooltip => {
-  $tooltipped.addEventListener('mouseenter', event => {
-    const { top, bottom, left, right } = $tooltipped.getBoundingClientRect()
+  $tooltip.addEventListener('mouseenter', event => {
+    const { top, bottom, left, right } = $tooltip.getBoundingClientRect()
 
-    console.log(`${top} px from the top of the window`)
-    console.log(
-      `${window.innerHeight - bottom}px from the bottom of the window`
-    )
-    console.log(`${left}px from the left of the window`)
-    console.log(`${window.innerWidth - right} from the right of the window`)
+    if (top > 20 && window.innerWidth - right > 120) {
+      event.target.setAttribute('data-tooltip', 'Above')
+      // event.target.classList.remove('bottom', 'left', 'right')
+      // event.target.classList.add('top')
+    } else if (bottom > 20 && window.innerWidth - right > 120) {
+      event.target.setAttribute('data-tooltip', 'Below')
+      // event.target.classList.remove('top', 'left', 'right')
+      // event.target.classList.add('bottom')
+    } else if (left > 120) {
+      event.target.setAttribute('data-tooltip', 'Left')
+      // event.target.classList.remove('top', 'bottom', 'right')
+      // event.target.classList.add('left')
+    }
+    // console.log(`${top} px from the top of the window`)
+    // console.log(`${window.innerHeight - bottom}px from the bottom of the window`)
+    // console.log(`${left}px from the left of the window`)
+    // console.log(`${window.innerWidth - right} from the right of the window`)
   })
 })
